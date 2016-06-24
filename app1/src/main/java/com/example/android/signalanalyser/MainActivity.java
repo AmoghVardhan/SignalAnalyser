eda7d7106091465145c8e13f16c52abfdef95369
Amogh <kashiamoghvardhan@gmail.com>
2016-06-24 20:35:00 +0530

    only thing left is to put the receive data in the graph

--- app1/src/main/java/com/example/android/signalanalyser/MainActivity.java
+++ app1/src/main/java/com/example/android/signalanalyser/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.signalanalyser;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
@@ -13,14 +14,21 @@ import android.graphics.DashPathEffect;
 import android.graphics.Paint;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.os.Handler;
+import android.os.Message;
 import android.provider.Settings;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -48,7 +56,77 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     String wName;
     String stredittext;
     GraphView graph;
-    private final static int REQUEST_ENABLE_BT = -1;
+//    private final static int REQUEST_ENABLE_BT = -1;
+
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";
+
+    private static final int REQUEST_CONNECT_DEVICE_SECURE = 1;
+    private static final int REQUEST_CONNECT_DEVICE_INSECURE = 2;
+    private static final int REQUEST_ENABLE_BT = 3;
+
+    private String connectedDeviceName = null;
+    private ArrayAdapter<String> chatArrayAdapter;
+
+    private StringBuffer outStringBuffer;
+    private BluetoothAdapter bluetoothAdapter = null;
+    private ChatService chatService = null;
+
+    private Handler handler = new Handler(new Handler.Callback() {
+
+        @Override
+        public boolean handleMessage(Message msg) {
+            switch (msg.what) {
+                case MESSAGE_STATE_CHANGE:
+                    switch (msg.arg1) {
+                        case ChatService.STATE_CONNECTED:
+                            setStatus(getString(R.string.title_connected_to,
+                                    connectedDeviceName));
+                            chatArrayAdapter.clear();
+                            break;
+                        case ChatService.STATE_CONNECTING:
+                            setStatus(R.string.title_connecting);
+                            break;
+                        case ChatService.STATE_LISTEN:
+                        case ChatService.STATE_NONE:
+                            setStatus(R.string.title_not_connected);
+                            break;
+                    }
+                    break;
+                case MESSAGE_WRITE:
+                    byte[] writeBuf = (byte[]) msg.obj;
+
+                    String writeMessage = new String(writeBuf);
+                    chatArrayAdapter.add("Me:  " + writeMessage);
+                    break;
+                case MESSAGE_READ:
+                    byte[] readBuf = (byte[]) msg.obj;
+
+                    String readMessage = new String(readBuf, 0, msg.arg1);
+                    chatArrayAdapter.add(connectedDeviceName + ":  " + readMessage);
+                    break;
+                case MESSAGE_DEVICE_NAME:
+
+                    connectedDeviceName = msg.getData().getString(DEVICE_NAME);
+                    Toast.makeText(getApplicationContext(),
+                            "Connected to " + connectedDeviceName,
+                            Toast.LENGTH_SHORT).show();
+                    break;
+                case MESSAGE_TOAST:
+                    Toast.makeText(getApplicationContext(),
+                            msg.getData().getString(TOAST), Toast.LENGTH_SHORT)
+                            .show();
+                    break;
+            }
+            return false;
+        }
+    });
 
 
 
@@ -154,12 +232,72 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         spinner6.setSelection(2);
         spinner6.setOnItemSelectedListener(this);
 
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+//        getWidgetReferences();
+//        bindEventHandler();
+
+        if (bluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth is not available",
+                    Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
     }
+
+
+//    private void bindEventHandler() {
+//        etMain.setOnEditorActionListener(mWriteListener);
+//
+//        btnSend.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                String message = etMain.getText().toString();
+//                sendMessage(message);
+//            }
+//        });
+//    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_CONNECT_DEVICE_SECURE:
+                if (resultCode == Activity.RESULT_OK) {
+                    connectDevice(data, true);
+                }
+                break;
+            case REQUEST_CONNECT_DEVICE_INSECURE:
+                if (resultCode == Activity.RESULT_OK) {
+                    connectDevice(data, false);
+                }
+                break;
+            case REQUEST_ENABLE_BT:
+                if (resultCode == Activity.RESULT_OK) {
+                    setupChat();
+                } else {
+                    Toast.makeText(this, R.string.bt_not_enabled_leaving,
+                            Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+        }
+    }
+
+    private void connectDevice(Intent data, boolean secure) {
+        String address = data.getExtras().getString(
+                BluetoothSettingsActivity.DEVICE_ADDRESS);
+        BluetoothDevice device = bluetoothAdapter.getRemoteDevice(address);
+        chatService.connect(device, secure);
+    }
+
 
 
     @Override
     protected void onResume() {
         super.onResume();
+        if (chatService != null) {
+            if (chatService.getState() == ChatService.STATE_NONE) {
+                chatService.start();
+            }
+        }
         // we're going to simulate real time with thread that append data to the graph
         new Thread(new Runnable() {
             @Override
@@ -185,44 +323,69 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        Intent serverIntent = null;
+        switch (item.getItemId()) {
+            case R.id.wifi_settings:
+                startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
 
-        //noinspection SimplifiableIfStatement
-
-        if (id == R.id.wifi_settings)
-        {
-            startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+            case R.id.secure_connect_scan:
+                serverIntent = new Intent(this, BluetoothSettingsActivity.class);
+                startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE_SECURE);
+                return true;
+            case R.id.insecure_connect_scan:
+                serverIntent = new Intent(this, BluetoothSettingsActivity.class);
+                startActivityForResult(serverIntent,
+                        REQUEST_CONNECT_DEVICE_INSECURE);
+                return true;
+            case R.id.discoverable:
+                ensureDiscoverable();
+                return true;
         }
-        if (id == R.id.bluetooth_settings)
-        {
-            Intent intent1 = new Intent(this,BluetoothSettingsActivity.class);
-            startActivityForResult(intent1,1);
-
-//            startActivity(new Intent(Settings.ACTION_BLUETOOTH_SETTINGS));
-        }
-//
-        return super.onOptionsItemSelected(item);
+        return false;
     }
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1) {
-            if(resultCode == RESULT_OK){
-                stredittext=data.getExtras().getString("rssi");
-                Toast.makeText(this,stredittext,Toast.LENGTH_SHORT).show();
-            }
+    private void ensureDiscoverable() {
+        if (bluetoothAdapter.getScanMode() != BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
+            Intent discoverableIntent = new Intent(
+                    BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            discoverableIntent.putExtra(
+                    BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+            startActivity(discoverableIntent);
         }
     }
+    private final void setStatus(int resId) {
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setSubtitle(resId);
+    }
+
+    private final void setStatus(CharSequence subTitle) {
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setSubtitle(subTitle);
+    }
+    private void setupChat() {
+        chatArrayAdapter = new ArrayAdapter<String>(this, R.layout.message);
+
+        chatService = new ChatService(this, handler);
+
+        outStringBuffer = new StringBuffer("");
+    }
+
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (requestCode == 1) {
+//            if(resultCode == RESULT_OK){
+//                stredittext=data.getExtras().getString("rssi");
+//                Toast.makeText(this,stredittext,Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//    }
 
     //    add random data
     private void addEntry() {
@@ -263,6 +426,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         BTAdapter.startDiscovery();
         registerReceiver(receiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
+//        String str = outStringBuffer.toString();
+//        int ri2 =Integer.parseInt(outStringBuffer.toString());
         series3.appendData(new DataPoint(lastX2++, rssi2), true, 10);
 
 
@@ -295,6 +460,35 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 //                }
 //            }
 //        };
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(
+                    BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+        } else {
+            if (chatService == null)
+                setupChat();
+        }
+    }
+    @Override
+    public synchronized void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (chatService != null)
+            chatService.stop();
     }
 
     @Override
